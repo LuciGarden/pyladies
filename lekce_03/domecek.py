26781ef5841931cfafe141640fef7c3bc2bb165f
Lucia Zahradníková <zahradnikova.luci@gmail.com>
2022-10-12 19:35:57 +0200

    Ignorování souboru

--- lekce_03/domecek.py
+++ lekce_03/domecek.py
@@ -1,11 +1,7 @@
 from turtle import fillcolor, begin_fill, forward, left, end_fill, exitonclick
 
 size = 100
-<<<<<<< HEAD
-color = "#purple"
-=======
 color = "#blue"
->>>>>>> sestiuhelnik
 
 # Nastaví barvu
 fillcolor(color)
